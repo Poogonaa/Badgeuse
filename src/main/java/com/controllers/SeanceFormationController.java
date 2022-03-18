1f01b7b227e82e893d63d35308296a8ce3c862de
Amina.E <elamina999@gmail.com>
2022-03-18 11:49:00 +0100

    il veut que je commit l'historique

--- src/main/java/com/controllers/SeanceFormationController.java
+++ src/main/java/com/controllers/SeanceFormationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("seancesformations")
+@RequestMapping("/seancesformations")
 public class SeanceFormationController {
 
     private final SeanceFormationServiceImpl seanceFormationService;
